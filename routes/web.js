c98f11e17e86809ebda4a897fac08103aabc3c86
Loyalcod <victorakujuobichibuike@gmail.com>
2023-04-17 12:27:27 -0700

    fourth commit

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,21 @@
 const {resolve} = require("path")
 
-const web = (app)=>{
+const web = (app,express)=>{
+    app.use(express.static(resolve('./home')))
+
 
     app.get('/',(req,res)=>{
-        const homePath = resolve('~home/index.html')
+        const homePath = resolve('./home/index.html')
+        res.sendFile(homePath)
+    })
+
+    app.get('/otp',(req,res)=>{
+        const homePath = resolve('./home/otp.html')
+        res.sendFile(homePath)
+    })
+
+    app.get('/chat',(req,res)=>{
+        const homePath = resolve('./home/chat.html')
         res.sendFile(homePath)
     })
 }
--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const apis = require("~routes/api")
 const socket = require("~sockets/socket")
 const app = express()
 require("dotenv").config()
-const {resolve} = require("path")
+
 
 /* ------------------------ import http from node js ------------------------ */
 const http = require('http').Server(app)
 /* ---------------------------- include socket io --------------------------- */
 const io = require('socket.io')(http)
 
-app.use(express.static(resolve('./home')))
+
 
 const port = process.env.PORT 
 
-web(app)
+web(app, express)
 apis(app)
 
 socket(io)
